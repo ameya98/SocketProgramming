7554465c2ea0a341897bdd5ecec70d86c31a8ea7
Ameya Daigavane <ameya.d.98@gmail.com>
2019-02-27 11:15:41 +0530

    Added simple load testing with multiprocessing + requests to spawn clients.

--- web-server/test-client.py
+++ web-server/test-client.py
@@ -1,16 +1,34 @@
 """
 Web Server Clients
 Author: Ameya Daigavane
-Uses cURL to make HTTP requests to our server.
+Spawns multiple clients that make HTTP requests to our server.
 """
 
-import subprocess
+import multiprocessing
 import os
+import requests
 
-# We remove our command-line proxy settings.
-env = dict(os.environ)
-env['HTTP_PROXY'] = ''
+# Remove command-line proxy settings.
+os.environ['HTTP_PROXY'] = ''
 
-# Subprocess to use curl and see the output.
-output = subprocess.check_output(['curl', '-s', 'localhost:7000/files/file1.txt'], env=env)
-print(output.decode())
+def start_client(client_num):
+
+    # Using the requests library to make HTTP requests.
+    response = requests.get('http://localhost:7000/files/file1.txt')
+
+    print(client_num, response.text)
+
+
+if __name__ == '__main__':
+    clients = []
+    num_clients = 100
+
+    for client_num in range(num_clients):
+        client_process = multiprocessing.Process(target=start_client, args=(client_num,))
+        client_process.start()
+        clients.append(client_process)
+
+    for client_process in clients:
+        client_process.join()
+
+
